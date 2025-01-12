f7f3b6c31692cfeb7ddcfd22200e3408a9836bba
Vageesha G <vageeshag75@gmail.com>
2025-01-12 12:47:36 +0000

    I made sure that footer always stays at the bottom by defining a new class called flex-wrapper and is added to header and footer.ejs file

--- index.js
+++ index.js
@@ -87,6 +87,13 @@ allBlogs.push({
     content: "We’re thrilled to have you here in our vibrant online community where stories, ideas, and knowledge come to life. Our platform is a haven for writers, thinkers, and readers who share a passion for creativity and expression. Whether you’re here to discover inspiring articles, share your own stories, or engage in meaningful conversations, you’ll find yourself at home in a space designed for exploration and connection.\n\nHere, you’ll uncover a wide variety of content ranging from thought-provoking blogs to helpful how-to guides and captivating personal narratives. Our community celebrates diversity, ensuring there’s something for everyone to enjoy and learn from. You can expect an easy-to-use interface, personalized experiences, and an inclusive environment where every voice matters.\n\nJoin us in shaping a thriving hub of ideas and inspiration! Create your own blogs, showcase your thoughts, or dive into the rich collection of posts shared by our talented community. Whether you’re here to connect, learn, or create, our platform is the perfect place to make your mark. Together, let’s build a space where words inspire action and ideas ignite change. Welcome aboard!"
 });
 
+allBlogs.push({
+    title: "The Art of Coding",
+    date: "12 Jan, 2025",
+    author: "Vageesha G",
+    content: "We’re thrilled to have you here in our vibrant online community where stories, ideas, and knowledge come to life. Our platform is a haven for writers, thinkers, and readers who share a passion for creativity and expression. Whether you’re here to discover inspiring articles, share your own stories, or engage in meaningful conversations, you’ll find yourself at home in a space designed for exploration and connection.\n\nHere, you’ll uncover a wide variety of content ranging from thought-provoking blogs to helpful how-to guides and captivating personal narratives. Our community celebrates diversity, ensuring there’s something for everyone to enjoy and learn from. You can expect an easy-to-use interface, personalized experiences, and an inclusive environment where every voice matters.\n\nJoin us in shaping a thriving hub of ideas and inspiration! Create your own blogs, showcase your thoughts, or dive into the rich collection of posts shared by our talented community. Whether you’re here to connect, learn, or create, our platform is the perfect place to make your mark. Together, let’s build a space where words inspire action and ideas ignite change. Welcome aboard!"
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
