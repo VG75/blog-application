86faf95a7cd3f0157f593f6f93e5acb66a66a725
Vageesha G <vageeshag75@gmail.com>
2025-01-12 12:34:38 +0000

    -Completed the Edit part in all-blogs where I route it and then after editing the data is sent to another route where in it handels the new content and replaces that with the old content.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { title } from "process";
 import { type } from "os";
+import { read } from "fs";
 
 const app = express();
 const port = 3000;
@@ -43,12 +44,29 @@ app.get("/view-blog", (req, res) => {
         content: formatedContent
     })
 });
-
+let blogEditIndex;
 app.post("/edit-blog", (req, res) => {
-    let blogIndex = parseInt(req.body["blogIndex"], 10);
-    res.render("edit-blog.ejs", {blog : allBlogs[blogIndex]});
+    blogEditIndex = parseInt(req.body["blogIndex"], 10);
+    res.render("edit-blog.ejs", {blog : allBlogs[blogEditIndex]});
+});
 
-}) 
+app.post("/edit-page", (req, res) => {
+    console.log(blogEditIndex);
+    const blogToEdit = allBlogs[blogEditIndex];
+    
+
+    blogToEdit.title = req.body["title"];
+    blogToEdit.author = req.body["author"];
+    blogToEdit.content = req.body["blogContent"];
+    
+    const date = new Date();
+    const dateStringList = date.toDateString().split(" ");
+    const currentDate = `${dateStringList[2]} ${dateStringList[1]}, ${dateStringList[3]}`;
+    blogToEdit.date = currentDate;
+    
+    res.redirect("/all-blogs");
+
+})
 
 
 
@@ -57,14 +75,14 @@ let date = new Date();
 const allBlogs = [];
 allBlogs.push({
     title: "The Art of Coding",
-    date: "12/01/2025",
+    date: "12 Jan, 2025",
     author: "Vageesha G",
     content: "We’re thrilled to have you here in our vibrant online community where stories, ideas, and knowledge come to life. Our platform is a haven for writers, thinkers, and readers who share a passion for creativity and expression. Whether you’re here to discover inspiring articles, share your own stories, or engage in meaningful conversations, you’ll find yourself at home in a space designed for exploration and connection.\n\nHere, you’ll uncover a wide variety of content ranging from thought-provoking blogs to helpful how-to guides and captivating personal narratives. Our community celebrates diversity, ensuring there’s something for everyone to enjoy and learn from. You can expect an easy-to-use interface, personalized experiences, and an inclusive environment where every voice matters.\n\nJoin us in shaping a thriving hub of ideas and inspiration! Create your own blogs, showcase your thoughts, or dive into the rich collection of posts shared by our talented community. Whether you’re here to connect, learn, or create, our platform is the perfect place to make your mark. Together, let’s build a space where words inspire action and ideas ignite change. Welcome aboard!"
 });
 
 allBlogs.push({
     title: "The Art of Coding",
-    date: "12/01/2025",
+    date: "12 Jan, 2025",
     author: "Vageesha G",
     content: "We’re thrilled to have you here in our vibrant online community where stories, ideas, and knowledge come to life. Our platform is a haven for writers, thinkers, and readers who share a passion for creativity and expression. Whether you’re here to discover inspiring articles, share your own stories, or engage in meaningful conversations, you’ll find yourself at home in a space designed for exploration and connection.\n\nHere, you’ll uncover a wide variety of content ranging from thought-provoking blogs to helpful how-to guides and captivating personal narratives. Our community celebrates diversity, ensuring there’s something for everyone to enjoy and learn from. You can expect an easy-to-use interface, personalized experiences, and an inclusive environment where every voice matters.\n\nJoin us in shaping a thriving hub of ideas and inspiration! Create your own blogs, showcase your thoughts, or dive into the rich collection of posts shared by our talented community. Whether you’re here to connect, learn, or create, our platform is the perfect place to make your mark. Together, let’s build a space where words inspire action and ideas ignite change. Welcome aboard!"
 });
